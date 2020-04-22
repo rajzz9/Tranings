0f48014fd5a2452b6c87b306dcb6d2d58d332ed2
rajzz9 <rajavardhan.merugu@gmail.com>
2020-04-22 12:12:23 +0530

    elk code added

---
+++ elkstack-example/src/test/java/com/example/elkstack/ElkstackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elkstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkstackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
