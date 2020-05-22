c291272a23121cc57476283b333f766f8aece2d4
rajzz9 <rajavardhan.merugu@gmail.com>
2020-05-22 12:28:32 +0530

    mysql configuration reading fromm config server example

---
+++ spring-cloud-config-server-client-mysql/client-mysql-db/src/main/java/com/example/client/mysqldb/ClientMysqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.client.mysqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMysqlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMysqlDbApplication.class, args);
+	}
+
+}
