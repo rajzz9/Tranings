662081622f6285a48a4df38b7461ecd573e74867
rajzz9 <rajavardhan.merugu@gmail.com>
2020-06-02 19:29:34 +0530

    jasper and file upload and download example

---
+++ spring-boot-jasper/src/main/java/com/example/demo/model/Report.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.context.annotation.Description;
+
+@Description(value = "Report Model Definition")
+public class Report implements Serializable {
+
+	private static final long serialVersionUID = 7526472295622776147L;
+
+	@NotNull
+	@NotBlank
+	private String firstName;
+	@NotNull
+	@NotBlank
+	private String lastName;
+	@NotNull
+	@NotBlank
+	private String age;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Report{" + "firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", age='" + age + '\''
+				+ '}';
+	}
+}
