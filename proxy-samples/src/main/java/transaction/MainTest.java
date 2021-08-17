e4f3f864d1b2dc7a33fbadc05584056b8338922e
rajzz9 <rajavardhan.merugu@gmail.com>
2021-08-17 23:21:17 +0530

    Proxy sample adding

--- proxy-samples/src/main/java/transaction/MainTest.java
+++ proxy-samples/src/main/java/transaction/MainTest.java
@@ -6,6 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainTest {
     private static Logger logger = Logger.getLogger(MainTest.class);
 
+    //https://spring.io/blog/2012/05/23/transactions-caching-and-aop-understanding-proxy-usage-in-spring
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("transaction/config-data-access.xml");
         AccountServiceNoImpl accountService = (AccountServiceNoImpl) applicationContext.getBean(AccountServiceNoImpl.class);
