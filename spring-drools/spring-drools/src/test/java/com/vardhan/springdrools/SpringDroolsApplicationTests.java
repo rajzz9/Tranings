92c57264f2a3846fdb28c1c306173e47be678de2
rajzz9 <rajavardhan.merugu@gmail.com>
2021-09-09 10:18:42 +0530

    spring drools rule engine code

---
+++ spring-drools/spring-drools/src/test/java/com/vardhan/springdrools/SpringDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vardhan.springdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
